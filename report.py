e392b6e01ddd9fd51d91f67241c55d5cb1508238
Daniele Simonetti <oppifjellet@gmail.com>
2014-08-23 15:35:34 +0200

    markdown content dump

--- report.py
+++ report.py
@@ -15,14 +15,64 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import dal 
+import dal
 import os
 
-# setup jinja2
-from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('l5rcm', 'templates'))
+try:
+    # setup jinja2
+    from jinja2 import Environment, PackageLoader
+    env = Environment(loader=PackageLoader('l5rcm', 'templates'))
 
-class ReportBuilder(object):
+    class ReportBuilder(object):
+        '''Build an HTML summary of the dal contents'''
+        def __init__(self, in_path, out_path):
+            self.out_path = out_path
+            self.in_path  = in_path
+            self.data     = None
+
+        def build(self):
+            # load _all_ the packs
+            self.data = dal.Data( [self.in_path] )
+
+            data_map = {}
+
+            core_pack = [ x for x in self.data.packs if x.id == 'core' ]
+            pack_list = sorted([ x for x in self.data.packs if x.id != 'core' ], cmp=lambda x,y: cmp(x.display_name, y.display_name) )
+
+            for p in self.data.packs:
+                # build content summary for each pack in different files
+                data_map[ p.id ] = self.get_pack_data(p)
+
+            # finally build the index
+            self.build_index(core_pack + pack_list, data_map)
+
+        def build_index(self, packs, pack_data):
+            o = os.path.join(self.out_path, "index.html")
+            tmpl = env.get_template('index.thtml')
+
+            with open(o, 'wt') as fobj:
+                fobj.write( tmpl.render(packs = packs, pack_data = pack_data) )
+
+        def build_pack_report(self, pack):
+            o = os.path.join(self.out_path, pack.id + ".html")
+            tmpl = env.get_template('pack.thtml')
+
+            # load a single package
+            dp = dal.Data( [pack.path] )
+            print(dp.__dict__)
+
+            with open(o, 'wt') as fobj:
+                fobj.write( tmpl.render(pack = pack, data = dp) )
+
+        def get_pack_data(self, pack):
+            # load a single package
+            return dal.Data( [pack.path] )
+except:
+    pass
+
+
+class ContentToMarkDown(object):
+
     '''Build an HTML summary of the dal contents'''
     def __init__(self, in_path, out_path):
         self.out_path = out_path
@@ -32,39 +82,35 @@ class ReportBuilder(object):
     def build(self):
         # load _all_ the packs
         self.data = dal.Data( [self.in_path] )
-        
-        data_map = {}
-        
-        core_pack = [ x for x in self.data.packs if x.id == 'core' ]
-        pack_list = sorted([ x for x in self.data.packs if x.id != 'core' ], cmp=lambda x,y: cmp(x.display_name, y.display_name) )
-        
-        for p in self.data.packs:
-            # build content summary for each pack in different files
-            data_map[ p.id ] = self.get_pack_data(p)
-	
-        # finally build the index
-        self.build_index(core_pack + pack_list, data_map)
 
-    def build_index(self, packs, pack_data):
-        o = os.path.join(self.out_path, "index.html")        
-        tmpl = env.get_template('index.thtml')
-        
-        with open(o, 'wt') as fobj:
-            fobj.write( tmpl.render(packs = packs, pack_data = pack_data) )
+        with open( self.out_path, 'wt' ) as self.fp:
+            self.build_clan_list    ()
+            #self.build_family_list  ()
+            #self.build_school_list  ()
+            #self.build_spell_list   ()
+            #self.build_skill_list   ()
+            #self.build_merit_list   ()
+            #self.build_flaw_list    ()
+            #self.build_kata_list    ()
+            #self.build_kiho_list    ()
 
-    def build_pack_report(self, pack):
-        o = os.path.join(self.out_path, pack.id + ".html")
-        tmpl = env.get_template('pack.thtml')
-        
-        # load a single package
-        dp = dal.Data( [pack.path] )
-        print(dp.__dict__)
-        
-        with open(o, 'wt') as fobj:
-            fobj.write( tmpl.render(pack = pack, data = dp) )     
-            
-    def get_pack_data(self, pack):
-        # load a single package
-        return dal.Data( [pack.path] )
-        
-        
+    def md_h1( self, text ):
+
+        self.fp.write( text + "\n" )
+        self.fp.write( "="*len(text) + "\n" )
+
+    def md_bullet( self, text ):
+        self.fp.write( "* " + text + "\n" )
+
+    def build_clan_list(self):
+
+        if len( self.data.clans ) == 0:
+            return
+
+        self.md_h1( "Clans" )
+
+        for c in self.data.clans:
+            self.md_bullet( c.name )
+
+
+
