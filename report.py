3be6f659b6c2e9118d1fc2e8007415f1d44a5746
OpenNingia <oppifjellet@gmail.com>
2015-08-25 15:40:31 +0200

    fix utf-8 encoding

--- report.py
+++ report.py
@@ -102,11 +102,12 @@ class ContentToMarkDown(object):
             self.build_kiho_list    ()
 
     def md_sethext( self, text, char ):
-        self.fp.write( u"\n" + text + u"\n" )
-        self.fp.write( char*len(text) + u"\n" )
+
+        self.fp.write(u"\n{0}\n".format(text).encode('UTF-8'))
+        self.fp.write(char*len(text) + u"\n")
 
     def md_atx( self, text, n ):
-        self.fp.write( u"\n{} {}\n".format( u"#"*n, text ) )
+        self.fp.write(u"\n{} {}\n".format(u"#"*n, text).encode('UTF-8'))
 
     def md_h1( self, text ):
         self.md_sethext( text, u'=' )
@@ -127,7 +128,9 @@ class ContentToMarkDown(object):
         self.md_atx( text, 6 )
 
     def md_bullet( self, text ):
-        self.fp.write( u"* " + text + u"\n" )
+        text_ = u"* {}\n".format(text)
+
+        self.fp.write(text_.encode('UTF-8'))
 
     def fmt_anchor( self, text, anchor ):
         return "[{}](#{})".format(text, anchor)
@@ -142,7 +145,6 @@ class ContentToMarkDown(object):
         for c in sorted( self.data.clans, key = lambda x: x.name ):
             self.md_bullet( c.name )
 
-        #self.fp.write( u"\n" )
 
     def build_family_list(self):
 
@@ -159,11 +161,9 @@ class ContentToMarkDown(object):
 
             for f in sorted(clan_families):
                 try:
-                    self.md_bullet( f.decode('utf-8') )
+                    self.md_bullet(f)
                 except:
                     print('cannot write', f)
-
-            #self.fp.write( u"\n" )
 
 
     def __build_school_list(self, caption, slist):
@@ -182,11 +182,10 @@ class ContentToMarkDown(object):
 
             for f in sorted(clan_schools):
                 try:
-                    self.md_bullet( f.decode('utf-8') )
+                    self.md_bullet(f)
                 except:
                     print('cannot write', f)
 
-            #self.fp.write( "\n" )
 
     def build_school_list(self):
 
@@ -217,9 +216,9 @@ class ContentToMarkDown(object):
 
             for f in sorted(skills):
                 try:
-                    self.md_bullet( f.decode('utf-8') )
-                except:
-                    print('cannot write', f)
+                    self.md_bullet(f)
+                except Exception as e:
+                    print('cannot write', f, e)
 
     def build_spell_list(self):
 
@@ -241,7 +240,7 @@ class ContentToMarkDown(object):
 
                     for f in sorted(spells_rank):
                         try:
-                            self.md_bullet( f.decode('utf-8') )
+                            self.md_bullet(f)
                         except:
                             print('cannot write', f)
 
@@ -259,7 +258,7 @@ class ContentToMarkDown(object):
 
             for f in sorted(perks):
                 try:
-                    self.md_bullet( f.decode('utf-8') )
+                    self.md_bullet(f)
                 except:
                     print('cannot write', f)
 
@@ -290,7 +289,7 @@ class ContentToMarkDown(object):
 
                     for f in sorted(powers_rank):
                         try:
-                            self.md_bullet( f.decode('utf-8') )
+                            self.md_bullet(f)
                         except:
                             print('cannot write', f)
 
