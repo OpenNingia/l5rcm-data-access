a69344963f63ad93a7cbd95351a26acf7738d508
OpenNingia <oppifjellet@gmail.com>
2016-06-01 10:52:42 +0200

    replaced filter with list comprehention

--- l5rdal/query.py
+++ l5rdal/query.py
@@ -15,24 +15,24 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-def get_clan(storage, id):
+def get_clan(storage, iid):
     try:
-        return filter( lambda x: x.id == id, storage.clans )[0]
+        return [x for x in storage.clans if x.id == iid][0]
     except:
         return None
-        
-def get_family(storage, id):
+
+def get_family(storage, iid):
     try:
-        return filter( lambda x: x.id == id, storage.families )[0]
+        return [x for x in storage.families if x.id == iid][0]
     except:
         return None
-        
-def get_school(storage, id):
+
+def get_school(storage, iid):
     try:
-        return filter( lambda x: x.id == id, storage.schools )[0]
+        return [x for x in storage.schools if x.id == iid][0]
     except:
         return None
-        
+
 def get_base_schools(storage):
     def is_base_school(school):
         return (len(school.require) == 0 and
@@ -42,98 +42,98 @@ def get_base_schools(storage):
         return [x for x in storage.schools if is_base_school(x)]
     except:
         return None
-        
+
 def get_school_tech(school_obj, rank):
     try:
-        return filter( lambda x: x.rank == rank, school_obj.techs )[0]
+        return [x for x in school_obj.techs if x.rank == rank][0]
     except:
         return None
-        
-def get_tech(storage, id):
-    for sc in storage.schools:
-        tech   = filter( lambda x: x.id == id, sc.techs )
-        if len(tech): return sc, tech[0]
-    return None, None 
 
-def get_skill(storage, id):
+def get_tech(storage, iid):
+    for sc in storage.schools:
+        techs = [x for x in sc.techs if x.id == iid]
+        if len(techs): return sc, techs[0]
+    return None, None
+
+def get_skill(storage, iid):
     try:
-        return filter( lambda x: x.id == id, storage.skills )[0]
+        return [x for x in storage.skills if x.id == iid][0]
     except:
         return None
 
 def get_skills(storage, categ):
-    return filter( lambda x: x.type == categ, storage.skills )
-        
+    return [x for x in storage.skills if x.type == categ]
+
 def get_spells(storage, ring, mastery):
-    return filter( lambda x: x.element == ring and x.mastery == mastery, storage.spells )
+    return [x for x in storage.spells if x.element == ring and x.mastery == mastery]
 
 def get_maho_spells(storage, ring, mastery):
-    return filter( lambda x: x.element == ring and x.mastery == mastery and 'maho' in x.tags, storage.spells )
-    
-def get_mastery_ability_rule(storage, id, value):
+    return [x for x in storage.spells if x.element == ring and x.mastery == mastery and 'maho' in x.tags]
+
+def get_mastery_ability_rule(storage, iid, value):
     try:
-        skill = get_skill(storage, id)
-        return filter( lambda x: x.rank == value, skill.mastery_abilities )[0].rule        
-    except:
-        return None    
-        
-def get_kata(storage, id):
-    try:
-        return [x for x in storage.katas if x.id == id][0]
-    except:
-        return None
-        
-def get_kiho(storage, id):
-    try:
-        return [x for x in storage.kihos if x.id == id][0]
-    except:
-        print(id)
-        return None        
-        
-def get_spell(storage, id):
-    try:
-        return filter( lambda x: x.id == id, storage.spells )[0]
+        skill = get_skill(storage, iid)
+        return [x for x in skill.mastery_abilities if x.rank == value][0].rule
     except:
         return None
 
-def get_merit(storage, id):
+def get_kata(storage, iid):
     try:
-        return [x for x in storage.merits if x.id == id][0]
+        return [x for x in storage.katas if x.id == iid][0]
     except:
         return None
-        
-def get_flaw(storage, id):
+
+def get_kiho(storage, iid):
     try:
-        return [x for x in storage.flaws if x.id == id][0]
+        return [x for x in storage.kihos if x.id == iid][0]
+    except:
+        print(id)
+        return None
+
+def get_spell(storage, iid):
+    try:
+        return [x for x in storage.spells if x.id == iid][0]
     except:
         return None
-        
+
+def get_merit(storage, iid):
+    try:
+        return [x for x in storage.merits if x.id == iid][0]
+    except:
+        return None
+
+def get_flaw(storage, iid):
+    try:
+        return [x for x in storage.flaws if x.id == iid][0]
+    except:
+        return None
+
 def get_weapon(storage, name):
     try:
         return [x for x in storage.weapons if x.name == name][0]
     except:
         return None
-        
+
 def get_armor(storage, name):
     try:
         return [x for x in storage.armors if x.name == name][0]
     except:
         return None
-        
-def get_weapon_effect(storage, id):
+
+def get_weapon_effect(storage, iid):
     try:
-        return [x for x in storage.weapon_effects if x.id == id][0]
-    except:
-        return None         
-        
-def get_ring(storage, id):
-    try:
-        return [x for x in storage.rings if x.id == id][0]
+        return [x for x in storage.weapon_effects if x.id == iid][0]
     except:
         return None
-        
-def get_trait(storage, id):
+
+def get_ring(storage, iid):
     try:
-        return [x for x in storage.traits if x.id == id][0]
+        return [x for x in storage.rings if x.id == iid][0]
     except:
-        return None        
+        return None
+
+def get_trait(storage, iid):
+    try:
+        return [x for x in storage.traits if x.id == iid][0]
+    except:
+        return None
